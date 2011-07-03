730a98e3a01919a9bc44e4f205d525cd07abe1f0
Stefan Richter <stefan@02strich.de>
2011-07-03 21:01:27 +0200

    new config system

--- pandora_gui/config.py
+++ pandora_gui/config.py
@@ -1,3 +1,10 @@
-PANDORA_USERNAME = ""
-PANDORA_PASSWORD = ""
-PANDORA_PROXY    = ""
+import ConfigParser
+import string
+
+config = ConfigParser.SafeConfigParser()
+config.read('config.ini')
+for section in config.sections():
+	for option in config.options(section):
+		name = string.upper("%s_%s" % (section, option))
+		#setattr(__package__, name, config.get(section, option))
+		vars()[name] = config.get(section, option)
